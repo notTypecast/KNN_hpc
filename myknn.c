7eaa04cdb44c5786348a0a6fdcc2e8d1115a6b75
notTypecast <ynkodizzy@gmail.com>
2023-01-22 19:24:42 +0200

    args

--- myknn.c
+++ myknn.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
 	//FILE *fpout = fopen("output.knn.txt","w");
 
 	int size, rank;
-	MPI_Init(NULL, NULL);
+	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
